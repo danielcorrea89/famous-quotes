a1a3ca166287506f411c64c3013ff13cab4bb28c
Daniel Correa <danielcorrea89@gmail.com>
2025-09-22 08:44:41 +1000

    add prevent_deletion_if_contains_resources = false as it was failing to destroy RG

--- infra/envs/dev/providers.tf
+++ infra/envs/dev/providers.tf
@@ -25,7 +25,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   subscription_id = "e7cbc1ca-744a-432a-b54b-dd8b5a2d2799"
   tenant_id       = "6f1b261a-5960-45a0-8020-a29b8000417a"
   use_cli         = true
